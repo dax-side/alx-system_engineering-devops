0967633e8fcc7bb48a5b99c3fc52236d6d0470aa
dax-side <damade615@gmail.com>
2022-10-17 00:21:31 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Manifest that installs puppet-lint version 2.5.0
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem'
+# installs the package puppet-lint
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip',
 }
